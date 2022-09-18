2d9ed4eb3e2b73123d9bd75ab25ff6f687975ff6
rastian24 <tharos24@gmail.com>
2022-09-18 05:02:27 -0300

    harvest from distance

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ function init(){
 
 function harvest(location) {
     if (spirit.energy < spirit.energy_capacity) {
-        spirit.move(location.position);
+        if (distance(spirit.position, location.position) > 150) {
+            spirit.move(location.position);
+        }
         spirit.energize(spirit)
     }
     if (spirit.energy == spirit.energy_capacity) {
@@ -15,10 +17,12 @@ function harvest(location) {
 
 function deposit(){
     if (spirit.energy > spirit.energy_capacity / 10){
-        spirit.move(my_base.position);
+        if (distance(spirit.position, my_base.position) > 150) {
+            spirit.move(my_base.position);
+        }
         spirit.energize(my_base);
     }
-    if (memory[spirit.id].task == "deposit" && spirit.energy <= spirit.energy_capacity / 10) {
+    if (spirit.energy <= spirit.energy_capacity / 10) {
         memory[spirit.id].task = "harvest";
         memory[spirit.id].location = get_harvest_loc();
     }
@@ -32,6 +36,13 @@ function get_harvest_loc(){
         return star_p89;
     }
 }
+
+function distance(a, b) {
+    d = Math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2);
+    return d;
+}
+
+//MAIN
 if (tick == 1) {
    init(); 
    console.log("Init");
@@ -45,6 +56,7 @@ for(i=0;i<my_spirits.length;i++){
         memory[spirit.id].task = "deposit";
         memory[spirit.id].location = my_star;
     }
+    console.log(distance(spirit.position, memory[spirit.id].location.position));
     if (memory[spirit.id].task == "harvest") {
        harvest(memory[spirit.id].location);
     }
@@ -54,6 +66,5 @@ for(i=0;i<my_spirits.length;i++){
     if (spirit.sight.enemies_beamable.length > 0){
         spirit.energize(spirit.sight.enemies_beamable[0]);
     }
-    console.log(memory[spirit.id].location.id);
 }
 
